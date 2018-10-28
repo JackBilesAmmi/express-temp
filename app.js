113140bdff61725e5000e41b3299d3d60509ecfb
JackBilesAmmi <36856350+JackBilesAmmi@users.noreply.github.com>
2018-10-28 13:07:57 +0000

    init
    First push

---
+++ app.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const os = require('os');
+const { body,validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
+const URL = require('url');
+const jsonScanner = require('./server/resources/json-scanner.js');
+const server = require('./server/express_webserver.js');
+
+var app = new server.EXPRESS().listen();
+
+app.get('/',function(req,res){
+  res.sendFile(__dirname+'/index.html');
+});
---
+++ server/express_webserver.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const exp = require('express');
+const bodyParser = require("body-parser");
+const path = require('path');
+const https = require('https');
+const http = require('http');
+var cookieParser = require('cookie-parser');
+
+function EXPRESS() {
+    this.port = 8080 || process.env.PORT;
+    this.app = exp();
+    this.app.use(bodyParser.json());
+    this.app.use(bodyParser.urlencoded({ extended: true }));
+    this.app.use(exp.static('client'));
+    this.app.use(cookieParser());
+};
+
+EXPRESS.prototype.listen = function(){
+    this.app.listen(this.port,function(){
+        console.log("Server up and listening");
+    });
+
+    return this.app;
+};
+function HTTP(){
+    var httpServer = http.createServer(app);
+    httpServer.listen(process.env.PORT || 3000, function() {
+    });
+    return http;
+}
+
+function HTTPS(){
+    var httpsServer = http.createServer(app);
+    httsServer.listen(process.env.PORT || 3000, function() {
+    });
+    return http;
+}
+
+module.exports.EXPRESS = EXPRESS;
+module.exports.HTTP = HTTP;
+module.exports.HTTPS = HTTPS;
