5d5a4bbc875a5f9cc34f1fa9509117e9da70f23d
Jack <jackbiles21@hotmail.co.uk>
2019-01-07 19:50:18 +0000

    JSON Scanner script update
    Improved scanning

--- server/resources/json-scanner.js
+++ server/resources/json-scanner.js
@@ -1,15 +1,22 @@
+function cleanseString(str){
+    return str.replace(/[^\w\s]/gi,'').toLowerCase()//Remove whitespace, special characters and convert to lower case
+}
 //return an array of objects according to key, value, or key and value matching
 function getObjects(obj, key, val) {
     var objects = [];
     for (var i in obj) {
         if (!obj.hasOwnProperty(i)) continue;
         if (typeof obj[i] == 'object') {
-            objects = objects.concat(getObjects(obj[i], key, val));    
+            objects = objects.concat(getObjects(obj[i], key, val));
         } else 
         //if key matches and value matches or if key matches and value is not passed (eliminating the case where key matches but passed value does not)
-        if (i == key && obj[i] == val || i == key && val == '') { //
+        
+        val = cleanseString(val);
+        key = cleanseString(key);
+        objVal = cleanseString(obj[i].toString());
+        if (cleanseString(i.toString()) == key && objVal == val || cleanseString(i.toString()) == key && val == '') { //
             objects.push(obj);
-        } else if (obj[i] == val && key == ''){
+        } else if (objVal == val && key == ''){
             //only add if the object is not already in the array
             if (objects.lastIndexOf(obj) == -1){
                 objects.push(obj);
@@ -26,7 +33,7 @@ function getValues(obj, key) {
         if (!obj.hasOwnProperty(i)) continue;
         if (typeof obj[i] == 'object') {
             objects = objects.concat(getValues(obj[i], key));
-        } else if (i == key) {
+        } else if (cleanseString(i.toString()) == cleanseString(key)) {
             objects.push(obj[i]);
         }
     }
@@ -40,7 +47,7 @@ function getKeys(obj, val) {
         if (!obj.hasOwnProperty(i)) continue;
         if (typeof obj[i] == 'object') {
             objects = objects.concat(getKeys(obj[i], val));
-        } else if (obj[i] == val) {
+        } else if (cleanseString(obj[i].toString()) == cleanseString(val)) {
             objects.push(i);
         }
     }
@@ -50,4 +57,3 @@ function getKeys(obj, val) {
 module.exports.getObjects = getObjects;
 module.exports.getValues = getValues;
 module.exports.getKeys = getKeys;
-
